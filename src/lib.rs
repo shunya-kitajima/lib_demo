4558d98fa1d7fae05f2e26ae9145024f4b76389f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-11-05 23:51:19 +0900

    add function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod generator;
+
+pub fn print_random_number() {
+    let n = generator::gen_ran();
+    println!("Random u8: {}", n);
 }
